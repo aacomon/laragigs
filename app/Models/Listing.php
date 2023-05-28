03c76a31f272abc9cedbd9195334d0fb34cff277
aacomon <comon.anthony@gmail.com>
2023-05-28 22:58:08 +0800

    update

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,32 +2,10 @@
 
 namespace App\Models;
 
-class Listing
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Listing extends Model
 {
-    public static function all()
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Latest Listings',
-                'description' => 'Some descriptions here'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Latest two',
-                'description' => 'Some descriptions here'
-            ]
-        ];
-    }
-
-    public static function find($id)
-    {
-        $listings = self::all();
-
-        foreach ($listings as $listing) {
-            if ($listing['id'] == $id) {
-                return $listing;
-            }
-        }
-    }
+    use HasFactory;
 }
